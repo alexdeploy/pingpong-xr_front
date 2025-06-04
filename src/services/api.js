8932750bd53366550a1d095f3607f69c9cb74a7c
Alejandro Soriano Rueda <asorianor@laberit.com>
2025-06-04 12:25:43 +0200

    prod url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:4000';
+const API_URL = 'https://pingpong-xrback-production.up.railway.app';
 
 export const fetchRanking = async () => {
   try {
